2be49077e942f13f5251f2c312f24d48452ce83f
Eric Wätke <ewaetke10@gmail.com>
2021-11-17 11:43:30 +0100

    🐛 Container Padding Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      padding: '2rem',
+    },
     fontFamily: {
       'sans': ['DM Sans'],
       'display': ['Sen']
