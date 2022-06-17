0a55a55e8134f39654bb057e6f16df0bb39fcf55
Eric Wätke <ewaetke10@gmail.com>
2022-06-17 15:01:14 +0200

    ✨ PWA

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,10 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Sen:wght@800&display=swap" rel="stylesheet" />
             <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital@0;1&display=swap" rel="stylesheet" /> 
+
+            <link rel="manifest" href="/manifest.json" />
+            <link rel="apple-touch-icon" href="/icon-apple-touch.png" />
+            <meta name="theme-color" content="#88E2A1" />
         </Head>
         {/* <body style={{background: '#E5E7E5'}}> */}
         <body>
