cec14dc22c6014d4a3a7a032bae1d57079d77459
Eric Wätke <ewaetke10@gmail.com>
2022-10-01 15:04:06 +0200

    💚 Revalidation

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -10,7 +10,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       // this should be the actual path not a rewritten path
       // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-      await res.revalidate('/path-to-revalidate')
+      await res.revalidate('/mensa/fhp/')
+      await res.revalidate('/mensa/neues-palais/')
+      await res.revalidate('/mensa/golm/')
       return res.json({ revalidated: true })
     } catch (err) {
       // If there was an error, Next.js will continue
