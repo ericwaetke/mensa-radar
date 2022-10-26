19e99bc402b54ddd0902e4789d4ce9b20a822e51
Eric Wätke <ewaetke10@gmail.com>
2022-10-26 13:20:42 +0200

    🐛 next/link updates

--- pages/mensa/[mensa]/[day]/index.tsx
+++ pages/mensa/[mensa]/[day]/index.tsx
@@ -123,13 +123,11 @@ export default function Mensa(
 			</style>
 
 			<div>
-				<Link href="/">
-					<a className="p-2 pl-0 flex items-center gap-4">
+				<Link href="/" className="p-2 pl-0 flex items-center gap-4">
 						<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M11.1426 6.75C11.5568 6.75 11.8926 6.41421 11.8926 6C11.8926 5.58579 11.5568 5.25 11.1426 5.25V6.75ZM0.326533 5.46967C0.0336397 5.76256 0.0336397 6.23744 0.326533 6.53033L5.0995 11.3033C5.3924 11.5962 5.86727 11.5962 6.16016 11.3033C6.45306 11.0104 6.45306 10.5355 6.16016 10.2426L1.91752 6L6.16016 1.75736C6.45306 1.46447 6.45306 0.989592 6.16016 0.696699C5.86727 0.403806 5.3924 0.403806 5.0995 0.696699L0.326533 5.46967ZM11.1426 5.25L0.856863 5.25V6.75L11.1426 6.75V5.25Z" fill="black"/>
 						</svg>
 						<h2 className="text-lg font-bold text-center w-full">{mensaName}</h2>
-					</a>
 				</Link>
 
 			</div>
@@ -144,7 +142,7 @@ export default function Mensa(
 							<p>
 							Heute hat die Mensa leider geschlossen. Möchtest du dir das Essen vom vergangenen Freitag anschauen?
 							</p>
-							<Link href={`/mensa/${mensa}/freitag`}>
+							<Link href={`/mensa/${mensa}/freitag`} legacyBehavior>
 								<a className="p-2 px-4 rounded-xl inline-flex items-center gap-4 border">
 									Zu vergangenem Freitag
 								</a>
@@ -222,7 +220,7 @@ export default function Mensa(
 			}
 			</motion.div>
         </div>
-    )
+    );
 }
 
 export function getStaticPaths() {
