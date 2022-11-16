7af4d542455bafa2dc054720eff8c5d83355c068
carl-qq <56872257+carl-qq@users.noreply.github.com>
2022-11-17 00:46:25 +0100

    💄 bar-bg removed, 🚧 barwidth

--- components/nutrients/nutrientComponent.tsx
+++ components/nutrients/nutrientComponent.tsx
@@ -37,14 +37,17 @@ export const NutrientComponent = ({nutrient}) => {
 
 	// In Float Percentage from 0 to 1
 	const barPosition = .1
+	const barWidth = nutrientData.reference / nutrientData.value > 10 ? 100 : nutrientData.reference / nutrientData.value * 10;
 
 	return (
 		<div className="w-5/12 flex flex-col space-y-1">
 			<p className="w-5/12 font-sans-med">{nutrient.value} {nutrient.unit}</p>
-			<div className='h-2 w-full bg-white relative rounded-full'>
+			<div className='h-2 w-full  rounded-full'>
 				<motion.div className='h-2 bg-main-green absolute rounded-full' 
 				initial={{width: 0}}
-				animate={{width: `${(nutrientData.reference / nutrientData.value) * 10}%`}}
+				animate={{width:
+					`${barWidth}%`
+				}}
 				transition={{duration: .5}}></motion.div>
 			</div>
 		</div>
