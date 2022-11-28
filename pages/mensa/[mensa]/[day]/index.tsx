88fe9e404d190e4d29dc0e0d538dc4eb163db28d
Eric Wätke <eric.waetke@fh-potsdam.de>
2022-11-28 08:59:38 +0100

    🐛 Show all offers + revalidation

--- pages/mensa/[mensa]/[day]/index.tsx
+++ pages/mensa/[mensa]/[day]/index.tsx
@@ -275,11 +275,9 @@ export default function Mensa(
 							{
 								// Show rest later
 								sortedFoodOffers?.map((offer, i) => {
-									if(!offer.sold_out){
-										return (
-											<Offer key={i} offer={offer} mensa={mensa} day={router.query.day} reff={el => visibleOffers.current[i] = el}/>
-										)
-									}
+									return (
+										<Offer key={i} offer={offer} mensa={mensa} day={router.query.day} reff={el => visibleOffers.current[i] = el}/>
+									)
 								})
 							}
 
