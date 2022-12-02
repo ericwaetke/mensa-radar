c20b0322a6775be47017df85c718880f040fbcc6
Eric Wätke <eric.waetke@fh-potsdam.de>
2022-12-02 11:00:34 +0100

    viewport fix?

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ class MyDocument extends Document {
             <link rel="manifest" href="/manifest.json" />
             <link rel="apple-touch-icon" href="/icon-apple-touch.png" />
             <meta name="theme-color" content="#E2EEE3" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="viewport" content="viewport-fit=cover" />
         </Head>
         {/* <body style={{background: '#E5E7E5'}}> */}
