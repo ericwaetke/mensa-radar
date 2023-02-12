fd9754cb85f4318821a05f414c82815a3a32939c
Eric Wätke <ewaetke10@gmail.com>
2023-02-12 17:48:45 +0100

    🎉 About and AI Image Gen

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -6,22 +6,22 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const {
 		query: { w, h, f, b, q, token },
 	} = req
- 
+
 	if (f && b) {
 		
 	const url = `${ process.env.NEXT_PUBLIC_SUPABASE_URL }/storage/v1/object/public/${ b }/${ f }?token=${ token }`
 	const buffer = (await axios({ url, responseType: "arraybuffer" })).data as Buffer
- 
+
 	// here we create a new_params object to convert string to number, and also set default value
-	 const new_params  = {
+	const new_params  = {
 	   w: + w || 800,  // set default 800px
 	   h: + h || null,    // set to null if not provided, so that Sharp automatically keep the aspect ratio
 	   q: + q || 80      // set default 80% quality
-	 }
- 
+	}
+
 	 // here's where the Transformation happens
 	sharp(buffer)
-	 	.rotate()
+		.rotate()
 		.resize({
 				width: new_params.w,
 				height: new_params.h,
@@ -40,11 +40,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 			res.setHeader("Content-Type", "image/webp")
 			res.end(data)
 		})
- 
-   } else {
-	 res.statusCode = 500
-	 res.setHeader("Content-Type", "text/html")
-	 res.end("<h1>Internal Error</h1><p>Sorry, there was a problem</p>")
-   }
- }
- 
+
+	} else {
+		res.statusCode = 500
+		res.setHeader("Content-Type", "text/html")
+		res.end("<h1>Internal Error</h1><p>Sorry, there was a problem</p>")
+	}
+}
