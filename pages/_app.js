425e9dac11f1f704ed51f7e1a382b1ead5ecffb9
Eric Wätke <ewaetke10@gmail.com>
2023-04-08 14:58:30 +0200

    🚑Analytics fixes again

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }) {
 		<NextNProgress color="#88E2A1"/>
 		<Toaster />
 
-		<PlausibleProvider domain="mensa-radar.de" customDomain="analytics.ericwaetke.de">
+		<PlausibleProvider domain="mensa-radar.de" customDomain="https://plausible.ericwaetke.de" selfHosted="true">
 			<Provider>
 				<Component {...pageProps} />
 			</Provider>
