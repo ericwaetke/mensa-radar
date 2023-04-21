b02aac4a58d10b91a7460d1a0fee11dd78bca196
Eric Wätke <ewaetke10@gmail.com>
2023-04-21 08:54:09 +0200

    🩹 ai thumbnail upload testing

--- pages/api/ai/uploadThumbnail.ts
+++ pages/api/ai/uploadThumbnail.ts
@@ -11,39 +11,42 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 			return;
 		}
 
-		const b64toBlob = (base64, type = 'image/png') => fetch(`data:${type};base64,${base64}`).then(res => res.blob())
-		const blob = await b64toBlob(base64)
+		// const b64toBlob = (base64, type = 'image/png') => fetch(`data:${type};base64,${base64}`).then(res => res.blob())
+		// const blob = await b64toBlob(base64)
 
-		await supabase
-			.storage
-			.from('ai-thumbnails')
-			.upload(`thumbnail_${foodId}.png`, blob)
-			.then(async _ => {
-				console.log("uploaded image to supabase")
-				await supabase
-					.from('food_offerings')
-					.update({ has_ai_thumbnail: true })
-					.eq('id', foodId)
-					.then(_ => {
-						console.log('success')
-					})
-			})
-			.catch(e => {
-				res.status(500).json({
-					error: e.json(),
-					message: "Error while uploading image to supabase"
-				});
-				console.log(e)
-				throw e
-			})
+		// await supabase
+		// 	.storage
+		// 	.from('ai-thumbnails')
+		// 	.upload(`thumbnail_${foodId}.png`, blob)
+		// 	.then(async _ => {
+		// 		console.log("uploaded image to supabase")
+		// 		await supabase
+		// 			.from('food_offerings')
+		// 			.update({ has_ai_thumbnail: true })
+		// 			.eq('id', foodId)
+		// 			.then(_ => {
+		// 				console.log('success')
+		// 			})
+		// 	})
+		// 	.catch(e => {
+		// 		res.status(500).json({
+		// 			error: e.json(),
+		// 			message: "Error while uploading image to supabase"
+		// 		});
+		// 		console.log(e)
+		// 		throw e
+		// 	})
 
 
 		res.status(200).json({
 			message: 'success',
+			foodId,
+			base64,
 		})
 	} catch (e) {
 		res.status(500).json({
-			e
+			e,
+			message: "Error destructuring body?",
 		});
 	}
 }
