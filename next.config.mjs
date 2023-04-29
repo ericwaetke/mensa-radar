5822d6182f509a289ce8c182ac647369cd893858
Eric Wätke <ewaetke10@gmail.com>
2023-04-29 10:51:49 +0200

    🐛 Runtime Caching Import

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const withPWA = nextPWA({
 	dest: "public",
 	register: true,
 	skipWaiting: true,
-	runtimeCaching,
+	runtimeCaching: nextPWA.runtimeCaching,
 	buildExcludes: [/middleware-manifest.json$/],
 });
 
