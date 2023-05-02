8efddc2c050c171c009b1509658142c79b002a14
Eric Wätke <eric.waetke@fh-potsdam.de>
2023-05-02 11:14:00 +0200

    ⬆️ Dependency Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 import "./src/env.mjs";
 /** @type {import('next').NextConfig} */
-// Import next-pwa 
+// Import next-pwa
 import nextPWA from "next-pwa";
 import runtimeCaching from "next-pwa/cache.js";
 
 const withPWA = nextPWA({
-	dest: "public",
-	register: true,
-	skipWaiting: true,
-	runtimeCaching,
-	disable: process.env.NODE_ENV === "development",
-	buildExcludes: [/middleware-manifest.json$/],
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: process.env.NODE_ENV === "development",
 });
 
 /**
@@ -19,30 +18,30 @@ const withPWA = nextPWA({
  * @constraint {{import('next').NextConfig}}
  */
 function defineNextConfig(config) {
-	return config;
+  return config;
 }
 
 const nextConfig = defineNextConfig({
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: 'localhost',
-				port: '3000',
-				pathname: '/api/image/**',
-			},
-			{
-				protocol: 'https',
-				hostname: '**.mensa-radar.de',
-			},
-			// Supabase Storage
-			{
-				protocol: 'https',
-				hostname: 'bqfzesnwsvziyglfeezk.supabase.co',
-				pathname: '/storage/**',
-			},
-		],
-	}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/image/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.mensa-radar.de',
+      },
+      // Supabase Storage
+      {
+        protocol: 'https',
+        hostname: 'bqfzesnwsvziyglfeezk.supabase.co',
+        pathname: '/storage/**',
+      },
+    ],
+  }
 });
 
 export default withPWA(nextConfig);
