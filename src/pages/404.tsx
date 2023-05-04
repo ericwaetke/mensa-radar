d28aea9e92f6047c13a93939e4cbe199cabf3bf0
Eric Wätke <eric.waetke@fh-potsdam.de>
2023-05-04 15:56:41 +0200

    custom error pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
+import Balancer from "react-wrap-balancer";
 
 export default function Custom404() {
-	return <div>
-		<h1>404 - Server-side error occurred</h1>
-		<Link href="/">
-			<a>Go back to the homepage</a>
-		</Link>
-	</div>
+  return <div className="flex flex-col h-screen w-screen items-center justify-center">
+    <div className="flex flex-col items-center">
+      <h1 className="block text-h1 font-serif-bold text-center">
+        <Balancer>
+          Wir konnten diese Mensa leider nicht finden!
+        </Balancer>
+      </h1>
+      <h2 className="font-serif-reg text-base text-center">Oder etwas ist in der URL kaputt gegangen</h2>
+      <Link href="/">
+        <a className="mt-4 font-semibold h-14 border min-w-fit grow rounded-lg flex justify-center items-center gap-2 cursor-pointer px-4">
+          Zurück zu Mensa Radar
+        </a>
+      </Link>
+    </div>
+  </div>
 }
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
+import Balancer from "react-wrap-balancer";
 
 export default function Custom500() {
-	return <div>
-		<h1>500 - Server-side error occurred</h1>
-		<Link href="/">
-			<a>Go back to the homepage</a>
-		</Link>
-	</div>
+  return <div className="flex flex-col h-screen w-screen items-center justify-center">
+    <div className="flex flex-col items-center">
+      <h1 className="block text-h1 font-serif-bold text-center">
+        <Balancer>
+          Es gab ein Problem dir das Essen zu zeigen...
+        </Balancer>
+      </h1>
+      <h2 className="font-serif-reg text-base text-center">Wahrscheinlich sind wir schon dabei, das Problem zu beseitigen!</h2>
+      <Link href="/">
+        <a className="mt-4 font-semibold h-14 border min-w-fit grow rounded-lg flex justify-center items-center gap-2 cursor-pointer px-4">
+          Zurück zu Mensa Radar
+        </a>
+      </Link>
+    </div>
+  </div>
 }
