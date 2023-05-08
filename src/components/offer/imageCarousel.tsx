8b9a4d054c54014472b5e089458c2cba2ae4fe7f
Eric Wätke <eric.waetke@fh-potsdam.de>
2023-05-08 16:00:18 +0200

    backdrop blur rendering delay

--- src/components/offer/imageCarousel.tsx
+++ src/components/offer/imageCarousel.tsx
@@ -1,31 +1,40 @@
 import Image from "next/image"
+import { useState } from "react"
 import { ImageComponent } from "./imageComponent"
 
 export const ImageCarousel = (
-	{ offer, tempImage, localAiThumbnail, aiThumbnailUrl, openImageFlow }: 
-	{ offer: FoodOffering, tempImage: string, localAiThumbnail: string, aiThumbnailUrl: string, openImageFlow: () => void}) => {
-	return (
-		<div className="w-full rounded-t-xl border-b border-gray/20 bg-lightshiny-green p-4">
-			<div className="h-min-20 m-auto rounded-lg">
-				<div onClick={() => openImageFlow()} className="relative overflow-x-auto flex gap-8 snap-x px-[25%] snap-mandatory rounded-lg">
+  { offer, tempImage, localAiThumbnail, aiThumbnailUrl, openImageFlow }:
+    { offer: FoodOffering, tempImage: string, localAiThumbnail: string, aiThumbnailUrl: string, openImageFlow: () => void }) => {
 
-					{
-						tempImage ? <ImageComponent type="user" src={tempImage} /> : null
-					}
-					{
-						offer.imageUrls.map((url, index) => {
-							return <ImageComponent type="user" src={url} key={index} />
-						})
-					}
-					{
-						offer.has_ai_thumbnail ? <ImageComponent type="ai" src={aiThumbnailUrl} /> : null
-					}
-					{
-						localAiThumbnail && localAiThumbnail !== "" && !offer.has_ai_thumbnail ? <ImageComponent type="ai" src={`data:image/png;base64,${localAiThumbnail}`} /> : null
-					}
+  const [imageAmount, setImageAmount] = useState<number>(
+    //ai images
+    (offer.has_ai_thumbnail || localAiThumbnail ? 1 : 0) +
+    // user images
+    (offer.imageUrls.length + (tempImage ? 1 : 0))
+  )
 
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className="w-full rounded-t-xl border-b border-gray/20 bg-lightshiny-green p-4">
+      <div className="h-min-20 m-auto rounded-lg">
+        <div onClick={() => openImageFlow()} className={`relative ${imageAmount > 1 ? "overflow-x-auto" : "overflow-hidden"} flex gap-8 snap-x px-[25%] snap-mandatory rounded-lg`}>
+
+          {
+            tempImage ? <ImageComponent type="user" src={tempImage} /> : null
+          }
+          {
+            offer.imageUrls.map((url, index) => {
+              return <ImageComponent type="user" src={url} key={index} />
+            })
+          }
+          {
+            offer.has_ai_thumbnail ? <ImageComponent type="ai" src={aiThumbnailUrl} /> : null
+          }
+          {
+            localAiThumbnail && localAiThumbnail !== "" && !offer.has_ai_thumbnail ? <ImageComponent type="ai" src={`data:image/png;base64,${localAiThumbnail}`} /> : null
+          }
+
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/offer/imageComponent.tsx
+++ src/components/offer/imageComponent.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image"
 
 export const ImageComponent = (
-	{type, src}:
-	{type: "user" | "ai", src: string}
-	) => {
+  { type, src }:
+    { type: "user" | "ai", src: string }
+) => {
 
-	return <div className="relative m-auto h-52 min-w-52 w-52 cursor-pointer snap-center shrink-0">
-		<Image src={src} className="h-full w-full rounded-lg object-cover" layout="fill" />
-		<span className="absolute left-2 top-2 flex space-x-1  rounded-full bg-white/60 px-2 py-1 font-sans-med text-xs text-black backdrop-blur">
-			{
-				type === "ai" ? <>
-					<svg role="img" xmlns="http://www.w3.org/2000/svg" className="h-full" width="16" height="16" viewBox="0 0 24 24" strokeWidth="1.5" aria-labelledby="boltIconTitle" stroke="currentColor" stroke-linecap="square" stroke-linejoin="miter" fill="none" color="#000"> <title id="boltIconTitle">Bolt</title> <path d="M5 14l8-11v7h5l-8 11v-7z" /> </svg>
-					<span>AI-generiert</span>
-				</> : <span>Nutzer:innen-Foto</span>
-			}
-		</span>
-		<span className="absolute bottom-2 right-2 flex h-12 w-12 items-center justify-center space-x-1 rounded-full bg-white font-sans-med text-white backdrop-blur">
-			<img alt="Eigenes Foto" src="/icons/camera.svg" className="w-6"></img>
-		</span>
-	</div>
+  return <div className="relative m-auto h-52 min-w-52 w-52 cursor-pointer snap-center shrink-0">
+    <Image src={src} className="h-full w-full rounded-lg object-cover" layout="fill" />
+    <span className="absolute left-2 top-2 flex space-x-1 rounded-full bg-white/60 px-2 py-1 font-sans-med text-xs text-black z-10
+    after:backdrop-blur after:content-[''] after:h-full after:w-full after:absolute after:top-0 after:left-0 after:rounded-full">
+      {
+        type === "ai" ? <>
+          <svg className="z-20 h-full" role="img" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" strokeWidth="1.5" aria-labelledby="boltIconTitle" stroke="currentColor" strokeLinecap="square" strokeLinejoin="miter" fill="none" color="#000"> <title id="boltIconTitle">Bolt</title> <path d="M5 14l8-11v7h5l-8 11v-7z" /> </svg>
+          <span className="z-20">AI-generiert</span>
+        </> : <span className="z-20">Nutzer:innen-Foto</span>
+      }
+    </span>
+    <span className="absolute bottom-2 right-2 flex h-12 w-12 items-center justify-center space-x-1 rounded-full bg-white font-sans-med text-white">
+      <img alt="Eigenes Foto" src="/icons/camera.svg" className="w-6"></img>
+    </span>
+  </div>
 }
