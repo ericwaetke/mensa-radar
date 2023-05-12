92d22448ebb819d28f859d619764625a49cbad02
Eric Wätke <ewaetke10@gmail.com>
2023-05-12 11:18:09 +0200

    🚧 more og meta tags

--- src/pages/mensa/[mensa]/[day]/[mealId].tsx
+++ src/pages/mensa/[mensa]/[day]/[mealId].tsx
@@ -27,16 +27,23 @@ export default function SharedMeal ({meal}: {meal: FoodOffering}) {
 	return <>
 		<Head>
 			<title>{meal.food_title} - Mensa Radar</title>
+			<meta property="twitter:domain" content="mensa-radar.de" />
 			<meta property="og:url" content={`https://mensa-radar.de/mensa/${mensa}/${day}`} /> :
+			<meta property="twitter:url" content={`https://mensa-radar.de/mensa/${mensa}/${day}`} />
 			<meta property="og:image" content={`${process.env.NODE_ENV === "development" ? "http://localhost:3000/" : "https://mensa-radar.de/"}api/og/singleMeal?id=${meal.id}`} />
+			<meta name="twitter:image" content={`${process.env.NODE_ENV === "development" ? "http://localhost:3000/" : "https://mensa-radar.de/"}api/og/singleMeal?id=${meal.id}`} />
 			<meta property="og:image:width" content="1200" />
 			<meta property="og:image:height" content="630" />
+			<meta name="twitter:card" content="summary_large_image" />
 			<meta property="og:image:alt" content={`${meal.food_title}`} />
 			<meta property="og:image:type" content="image/png" />
 			<meta property="og:title" content={`${meal.food_title}`} />
+			<meta name="twitter:title" content={`${meal.food_title}`} />
 			<meta property="og:site_name" content="Mensa Radar" />
 			<meta property="og:description" content={`${foodType} - Essen vom ${meal.date} in der Mensa ${meal.mensa}!`} />
+			<meta name="twitter:description" content={`${foodType} - Essen vom ${meal.date} in der Mensa ${meal.mensa}!`} />
 			<meta property="og:type" content="website" />
+
 		</Head>
 	</>
 }
