490acac39ed317358101afb6e9def707457e52eb
Eric Wätke <eric.waetke@fh-potsdam.de>
2023-11-11 14:55:04 +0100

    json formatting

--- src/pages/api/ai/generateThumbnail.ts
+++ src/pages/api/ai/generateThumbnail.ts
@@ -40,169 +40,169 @@ const GenerateThumbnail = async (req: NextApiRequest, res: NextApiResponse) => {
 					},
 					"class_type": "CheckpointLoaderSimple"
 				},
-			  "5": {
-				"inputs": {
-				  "width": 1024,
-				  "height": 1024,
-				  "batch_size": 1
+				"5": {
+					"inputs": {
+						"width": 1024,
+						"height": 1024,
+						"batch_size": 1
+					},
+					"class_type": "EmptyLatentImage"
 				},
-				"class_type": "EmptyLatentImage"
-			  },
-			  "8": {
-				"inputs": {
-				  "samples": [
-					"23",
-					0
-				  ],
-				  "vae": [
-					"4",
-					2
-				  ]
+				"8": {
+					"inputs": {
+						"samples": [
+							"23",
+							0
+						],
+						"vae": [
+							"4",
+							2
+						]
+					},
+					"class_type": "VAEDecode"
 				},
-				"class_type": "VAEDecode"
-			  },
-			  "10": {
-				"inputs": {
-				  "ckpt_name": "sd_xl_base_1.0.safetensors"
+				"10": {
+					"inputs": {
+						"ckpt_name": "sd_xl_base_1.0.safetensors"
+					},
+					"class_type": "CheckpointLoaderSimple"
 				},
-				"class_type": "CheckpointLoaderSimple"
-			  },
-			  "22": {
-				"inputs": {
-				  "add_noise": "enable",
-				  "noise_seed": 454,
-				  "steps": 20,
-				  "cfg": 7.5,
-				  "sampler_name": "ddim",
-				  "scheduler": "normal",
-				  "start_at_step": 0,
-				  "end_at_step": 20,
-				  "return_with_leftover_noise": "enable",
-				  "model": [
-					"10",
-					0
-				  ],
-				  "positive": [
-					"75",
-					0
-				  ],
-				  "negative": [
-					"82",
-					0
-				  ],
-				  "latent_image": [
-					"5",
-					0
-				  ]
+				"22": {
+					"inputs": {
+						"add_noise": "enable",
+						"noise_seed": 454,
+						"steps": 20,
+						"cfg": 7.5,
+						"sampler_name": "ddim",
+						"scheduler": "normal",
+						"start_at_step": 0,
+						"end_at_step": 20,
+						"return_with_leftover_noise": "enable",
+						"model": [
+							"10",
+							0
+						],
+						"positive": [
+							"75",
+							0
+						],
+						"negative": [
+							"82",
+							0
+						],
+						"latent_image": [
+							"5",
+							0
+						]
+					},
+					"class_type": "KSamplerAdvanced"
 				},
-				"class_type": "KSamplerAdvanced"
-			  },
-			  "23": {
-				"inputs": {
-				  "add_noise": "disable",
-				  "noise_seed": 454,
-				  "steps": 20,
-				  "cfg": 7.5,
-				  "sampler_name": "euler",
-				  "scheduler": "normal",
-				  "start_at_step": 20,
-				  "end_at_step": 1000,
-				  "return_with_leftover_noise": "disable",
-				  "model": [
-					"4",
-					0
-				  ],
-				  "positive": [
-					"120",
-					0
-				  ],
-				  "negative": [
-					"81",
-					0
-				  ],
-				  "latent_image": [
-					"22",
-					0
-				  ]
+				"23": {
+					"inputs": {
+						"add_noise": "disable",
+						"noise_seed": 454,
+						"steps": 20,
+						"cfg": 7.5,
+						"sampler_name": "euler",
+						"scheduler": "normal",
+						"start_at_step": 20,
+						"end_at_step": 1000,
+						"return_with_leftover_noise": "disable",
+						"model": [
+							"4",
+							0
+						],
+						"positive": [
+							"120",
+							0
+						],
+						"negative": [
+							"81",
+							0
+						],
+						"latent_image": [
+							"22",
+							0
+						]
+					},
+					"class_type": "KSamplerAdvanced"
 				},
-				"class_type": "KSamplerAdvanced"
-			  },
-			  "75": {
-				"inputs": {
-				  "width": 2048,
-				  "height": 2048,
-				  "crop_w": 0,
-				  "crop_h": 0,
-				  "target_width": 2048,
-				  "target_height": 2048,
-				  "text_g": "An image of the dish ${translatedFoodTitle}",
-				  "text_l": "centered, head on, canteen, close up, realistic, real food, high detail, canteen food, professional food photography, single plate",
-				  "clip": [
-					"10",
-					1
-				  ]
+				"75": {
+					"inputs": {
+						"width": 2048,
+						"height": 2048,
+						"crop_w": 0,
+						"crop_h": 0,
+						"target_width": 2048,
+						"target_height": 2048,
+						"text_g": "An image of the dish ${translatedFoodTitle}",
+						"text_l": "centered, head on, canteen, close up, realistic, real food, high detail, canteen food, professional food photography, single plate",
+						"clip": [
+							"10",
+							1
+						]
+					},
+					"class_type": "CLIPTextEncodeSDXL"
 				},
-				"class_type": "CLIPTextEncodeSDXL"
-			  },
-			  "81": {
-				"inputs": {
-				  "ascore": 2,
-				  "width": 2048,
-				  "height": 2048,
-				  "text": "noise, grit, dull, washed out, low contrast, blurry, hazy, malformed, warped, deformed, cartoon, illustration",
-				  "clip": [
-					"4",
-					1
-				  ]
+				"81": {
+					"inputs": {
+						"ascore": 2,
+						"width": 2048,
+						"height": 2048,
+						"text": "noise, grit, dull, washed out, low contrast, blurry, hazy, malformed, warped, deformed, cartoon, illustration",
+						"clip": [
+							"4",
+							1
+						]
+					},
+					"class_type": "CLIPTextEncodeSDXLRefiner"
 				},
-				"class_type": "CLIPTextEncodeSDXLRefiner"
-			  },
-			  "82": {
-				"inputs": {
-				  "width": 2048,
-				  "height": 2048,
-				  "crop_w": 0,
-				  "crop_h": 0,
-				  "target_width": 2048,
-				  "target_height": 2048,
-				  "text_g": "noise, grit, dull, washed out, low contrast, blurry, hazy, malformed, warped, deformed, cartoon, illustration",
-				  "text_l": "noise, grit, dull, washed out, low contrast, blurry, hazy, malformed, warped, deformed, cartoon, illustration",
-				  "clip": [
-					"10",
-					1
-				  ]
+				"82": {
+					"inputs": {
+						"width": 2048,
+						"height": 2048,
+						"crop_w": 0,
+						"crop_h": 0,
+						"target_width": 2048,
+						"target_height": 2048,
+						"text_g": "noise, grit, dull, washed out, low contrast, blurry, hazy, malformed, warped, deformed, cartoon, illustration",
+						"text_l": "noise, grit, dull, washed out, low contrast, blurry, hazy, malformed, warped, deformed, cartoon, illustration",
+						"clip": [
+							"10",
+							1
+						]
+					},
+					"class_type": "CLIPTextEncodeSDXL"
 				},
-				"class_type": "CLIPTextEncodeSDXL"
-			  },
-			  "120": {
-				"inputs": {
-				  "ascore": 6,
-				  "width": 2048,
-				  "height": 2048,
-				  "text": "centered, head on, canteen, close up, realistic, real food, high detail, canteen food, professional food photography, single plate",
-				  "clip": [
-					"4",
-					1
-				  ]
+				"120": {
+					"inputs": {
+						"ascore": 6,
+						"width": 2048,
+						"height": 2048,
+						"text": "centered, head on, canteen, close up, realistic, real food, high detail, canteen food, professional food photography, single plate",
+						"clip": [
+							"4",
+							1
+						]
+					},
+					"class_type": "CLIPTextEncodeSDXLRefiner"
 				},
-				"class_type": "CLIPTextEncodeSDXLRefiner"
-			  },
-			  "184": {
-				"inputs": {
-				  "filename_prefix": "ComfyUI",
-				  "images": [
-					"8",
-					0
-				  ]
-				},
-				"class_type": "SaveImage"
-			  }
+				"184": {
+					"inputs": {
+						"filename_prefix": "ComfyUI",
+						"images": [
+							"8",
+							0
+						]
+					},
+					"class_type": "SaveImage"
+				}
 			}
-		  }`
+		}`
 
-		const webSocket = new WebSocket("ws://127.0.0.1:8188/ws?client=MensaRadarInstance");
+		const webSocket = new WebSocket("ws://ai.ericwaetke.de:8188/ws?client=MensaRadarInstance");
 
-		const promtResponse: PromptResponse = await fetch('http://127.0.0.1:8188/prompt', { method: 'POST', body: prompt }).then(res => { console.log(res); return res.json() }).catch(e => console.log(e))
+		const promtResponse: PromptResponse = await fetch('http://ai.ericwaetke.de:8188/prompt', { method: 'POST', body: prompt }).then(res => { console.log(res); return res.json() }).catch(e => console.log(e))
 
 		let progress = 0;
 		webSocket.onmessage = (event) => {
@@ -217,12 +217,13 @@ const GenerateThumbnail = async (req: NextApiRequest, res: NextApiResponse) => {
 		};
 
 		async function getImage() {
-			const history = await fetch(`http://127.0.0.1:8188/history/${promtResponse.prompt_id}/`).then({
+			const history = await fetch(`http://ai.ericwaetke.de:8188/history/${promtResponse.prompt_id}/`).then(res => {
+				console.log(res)
 				return res => res.json()
 			}).catch(e => console.log(e))
 			const { filename, subfolder, type } = history[promtResponse.prompt_id].outputs["184"][0]
 			console.log(filename, subfolder, type)
-			const image = await fetch(`http://127.0.0.1:8188/view?filename=${filename}&subfolder=${subfolder}&type=${type}`).then(res => res.blob())
+			const image = await fetch(`http://ai.ericwaetke.de:8188/view?filename=${filename}&subfolder=${subfolder}&type=${type}`).then(res => res.blob())
 			res.status(200).json({
 				message: 'success',
 				base64: URL.createObjectURL(image)
