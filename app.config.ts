585480294b106a74259312d7b0fb9e9454530fbe
Eric Wätke <ewaetke10@gmail.com>
2024-10-07 16:44:17 +0200

    Added Fresh Solid Start instance

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "@solidjs/start/config";
+
+export default defineConfig({
+  vite: {
+    server: {
+      port: 3333,
+    },
+    ssr: { external: ["drizzle-orm"] },
+  },
+});
---
+++ drizzle.config.ts
@@ -0,0 +1,8 @@
+export default {
+  dialect: "postgresql",
+  schema: "./drizzle/schema.ts",
+  out: "./drizzle/migrations/",
+  dbCredentials: {
+    url: process.env.DATABASE_URI,
+  },
+};
