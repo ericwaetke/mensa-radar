60988eb895b1e14c390bcbe38748049f55a0e112
Eric Wätke <ewaetke10@gmail.com>
2024-12-02 10:01:30 +0100

    chore: Updated DB Env Vars

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,18 @@
+import { readFileSync } from "fs";
+
+const databasePassword = process.env.DATABASE_PASSWORD_FILE
+  ? readFileSync(process.env.DATABASE_PASSWORD_FILE, "utf8")
+  : undefined;
+
 export default {
-	dialect: 'postgresql',
-	schema: './drizzle/schema.ts',
-	out: './drizzle/migrations/',
-	dbCredentials: {
-		url: process.env.DATABASE_URI,
-	},
+  dialect: 'postgresql',
+  schema: './drizzle/schema.ts',
+  out: './drizzle/migrations/',
+  dbCredentials: {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: databasePassword,
+    database: process.env.POSTGRES_DB,
+  },
 }
