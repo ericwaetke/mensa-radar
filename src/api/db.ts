81aef60e9aee0aa0e6c70d3d51c4e99a2dabf53f
Eric Wätke <ewaetke10@gmail.com>
2024-12-02 10:45:24 +0100

    fix: Set Correct Env Vars

--- src/api/db.ts
+++ src/api/db.ts
@@ -1,8 +1,13 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from '../../drizzle/schema'
+import { readFileSync } from 'fs';
 
-const queryClient = postgres(
-	'postgres://mensauser:postgres@127.0.0.1:5432/mensahhub',
-)
+const databasePassword = process.env.POSTGRES_PASSWORD_FILE
+  ? readFileSync(process.env.POSTGRES_PASSWORD_FILE, "utf8")
+  : undefined;
+const databaseURI = `postgres://${process.env.POSTGRES_USER}:${databasePassword}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
+console.log("databaseURI: ", databaseURI);
+
+const queryClient = postgres(databaseURI)
 export const db = drizzle(queryClient, { schema })
