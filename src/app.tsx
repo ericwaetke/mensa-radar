ccaea6b004e7fff34b86cfb903fb97868c5c1027
Eric Wätke <ewaetke10@gmail.com>
2024-12-02 10:30:13 +0100

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -8,9 +8,6 @@ import '@fontsource-variable/noto-sans'
 import { readFileSync } from 'fs'
 
 export default function App() {
-  console.log(process.env.POSTGRES_PASSWORD_FILE
-    ? readFileSync(process.env.POSTGRES_PASSWORD_FILE, "utf8")
-    : undefined)
   return (
     <Router
       root={(props) => (
