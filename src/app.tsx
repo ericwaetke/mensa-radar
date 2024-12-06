ec74f44185a974ca152433be85bbbb77e610001d
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-12-06 21:23:09 +0100

    chore: Added solidjs/meta

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,20 @@
 // @refresh reload
-import { Router } from '@solidjs/router'
-import { FileRoutes } from '@solidjs/start/router'
-import { Suspense } from 'solid-js'
-import './app.css'
-import './bespoke-slab.css'
-import '@fontsource-variable/noto-sans'
-import { readFileSync } from 'fs'
+import { Router } from "@solidjs/router"
+import { FileRoutes } from "@solidjs/start/router"
+import { Suspense } from "solid-js"
+import "./app.css"
+import "./bespoke-slab.css"
+import "@fontsource-variable/noto-sans"
 
 export default function App() {
-  return (
-    <Router
-      root={(props) => (
-        <>
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  )
+	return (
+		<Router
+			root={(props) => (
+				<>
+					<Suspense>{props.children}</Suspense>
+				</>
+			)}>
+			<FileRoutes />
+		</Router>
+	)
 }
