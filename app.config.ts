1f34dbfc7298d374a266f072d8e3465b096d92bb
Eric Wätke <eric.waetke@fh-potsdam.de>
2024-12-18 16:12:47 +0100

    chore: removed deno server

--- app.config.ts
+++ app.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
     },
     ssr: { external: ['drizzle-orm'] },
   },
-  server: {
-    preset: 'deno-server',
-  },
+  // server: {
+  //   preset: '',
+  // },
   devOverlay: false
 })
