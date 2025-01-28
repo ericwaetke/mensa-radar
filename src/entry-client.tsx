534f3fad4f44ae210cbcd3a7c23529613b8364a3
Eric Wätke <eric.waetke@fh-potsdam.de>
2025-01-28 21:49:59 +0100

    feat: added sentry

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,12 @@
 // @refresh reload
-import { mount, StartClient } from '@solidjs/start/client'
+import * as Sentry from "@sentry/solidstart"
+import { solidRouterBrowserTracingIntegration } from "@sentry/solidstart/solidrouter"
+import { mount, StartClient } from "@solidjs/start/client"
 
-mount(() => <StartClient />, document.getElementById('app')!)
+Sentry.init({
+	dsn: "http://35b2ef5a19fd4d61b8a74bbd88b0e30f@glitchtip.woven.design/1",
+	integrations: [solidRouterBrowserTracingIntegration()],
+	tracesSampleRate: 1.0, //  Capture 100% of the transactions
+})
+
+mount(() => <StartClient />, document.getElementById("app")!)
