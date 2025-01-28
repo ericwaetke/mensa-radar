0cc6b8a93ee2ed07d70447153c4f7734c5b81722
Eric Wätke <eric.waetke@fh-potsdam.de>
2025-01-28 22:30:40 +0100

    ??

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,6 +1,13 @@
 // @refresh reload
 import { createHandler, StartServer } from "@solidjs/start/server"
 
+import * as Sentry from "@sentry/solidstart"
+
+Sentry.init({
+	dsn: "http://35b2ef5a19fd4d61b8a74bbd88b0e30f@glitchtip.woven.design/1",
+	tracesSampleRate: 1.0, //  Capture 100% of the transactions
+})
+
 export default createHandler(() => (
 	<StartServer
 		document={({ assets, children, scripts }) => (
